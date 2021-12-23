59ef48fdd6354455c631f2808709fe7ba47e3cdc
Scott Schroeder <scottschroeder@sent.com>
2021-12-22 20:31:28 -0500

    wip - seven segment solving

---
+++ src/challenge/day8/constraint.rs
@@ -0,0 +1,5 @@
+use aoc::ac3::Constraint;
+use aoc::ac3::AC3;
+use super::sevensegment::SEGMENTS_LEN;
+use super::sevensegment::SevenSegment;
+
--- src/challenge/day8/sevensegment.rs
+++ src/challenge/day8/sevensegment.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 
-const SEGMENTS_LEN: usize = 7;
+pub const SEGMENTS_LEN: usize = 7;
 
 /*
      aaaa
@@ -28,6 +28,21 @@ fn wire_name_to_pos(c: char) -> Result<usize> {
 }
 
 impl SevenSegment {
+    pub fn from_last_digit(d: u8) -> SevenSegment {
+        SevenSegment(match d % 10 {
+            0 => [true, true, true, false, true, true, true],
+            1 => [false, false, true, false, false, true, false],
+            2 => [true, false, true, true, true, false, true],
+            3 => [true, false, true, true, false, true, true],
+            4 => [false, true, true, true, false, true, false],
+            5 => [true, true, false, true, false, true, true],
+            6 => [true, true, false, true, true, true, true],
+            7 => [true, false, true, false, false, true, false],
+            8 => [true, true, true, true, true, true, true],
+            9 => [true, true, true, true, false, true, true],
+            _ => unreachable!("invalid digit"),
+        })
+    }
     pub fn from_wire_code(code: &str) -> Result<SevenSegment> {
         let mut disp = SevenSegment::default();
         for idx in code.chars().map(wire_name_to_pos) {
@@ -36,13 +51,19 @@ impl SevenSegment {
         }
         Ok(disp)
     }
-    fn lit_segments(self) -> usize {
-        self.0.iter().filter(|s| **s).count()
+    fn lit_segments(&self) -> impl Iterator<Item = usize> + '_ {
+        self.0
+            .iter()
+            .enumerate()
+            .filter_map(|(idx, s)| if *s { Some(idx) } else { None })
+    }
+    fn total_lit_segments(self) -> usize {
+        self.lit_segments().count()
     }
 }
 
 pub fn guess_digit(s: SevenSegment) -> Option<i64> {
-    match s.lit_segments() {
+    match s.total_lit_segments() {
         2 => Some(1),
         4 => Some(4),
         3 => Some(7),
